91486ac0af13c7448c57ffe1e689142940e061b1
Jorge De Los Santos <jorgeluk13@gmail.com>
2019-09-30 12:06:51 +0200

    Migrated from Gson to Moshi.

--- data/src/main/java/jdls/one/data/service/MoviesServiceFactory.kt
+++ data/src/main/java/jdls/one/data/service/MoviesServiceFactory.kt
@@ -1,8 +1,7 @@
 package jdls.one.data.service
 
-import com.google.gson.FieldNamingPolicy
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import io.reactivex.Single
 import jdls.one.data.model.RawMovieSearchResults
 import okhttp3.Interceptor
@@ -10,7 +9,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.util.concurrent.TimeUnit
@@ -23,15 +22,15 @@ object MoviesServiceFactory {
       makeApiKeyInterceptor(apiKey),
       makeLoggingInterceptor(isDebug)
     )
-    return makeMoviesService(okHttpClient, makeGson())
+    return makeMoviesService(okHttpClient, makeMoshi())
   }
 
-  private fun makeMoviesService(okHttpClient: OkHttpClient, gson: Gson): MoviesService {
+  private fun makeMoviesService(okHttpClient: OkHttpClient, moshi: Moshi): MoviesService {
     val retrofit = Retrofit.Builder()
       .baseUrl("https://api.themoviedb.org/3/")
       .client(okHttpClient)
       .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-      .addConverterFactory(GsonConverterFactory.create(gson))
+      .addConverterFactory(MoshiConverterFactory.create(moshi))
       .build()
     return retrofit.create(MoviesService::class.java)
   }
@@ -48,12 +47,7 @@ object MoviesServiceFactory {
       .build()
   }
 
-  private fun makeGson(): Gson {
-    return GsonBuilder()
-      .setLenient()
-      .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-      .create()
-  }
+  private fun makeMoshi(): Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
   private fun makeLoggingInterceptor(isDebug: Boolean): HttpLoggingInterceptor {
     val logging = HttpLoggingInterceptor()
